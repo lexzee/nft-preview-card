bdc816555a38fc8ca7685c430767edeb15033115
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-10 16:49:10 +0100

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+      Equilibrium #3429
+
+Our Equilibrium collection promotes balance and calm.
+
+0.041 ETH
+3 days left
+
+Creation of Jules Wyvern
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge"      target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
