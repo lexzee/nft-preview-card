26f121af38c3ed358e2f1f0f62d73c6e649b8843
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-11 09:37:56 +0100

    basic Html struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import dImg from "./images/image-equilibrium.jpg"
+import ethIcon from './images/icon-ethereum.svg'
+import clock from './images/icon-clock.svg'
+import avatar from './images/image-avatar.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,18 +11,35 @@ function App() {
   return (
     <>
       <div className="App">
-      Equilibrium #3429
+        <img src={dImg} alt="Equilibrium" className='nftImg' />
+        <div className="cardDetails">
+          <h1 className="title">
+          Equilibrium #3429
+          </h1>
+          <p className="description">
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+          <div className="priceDuration">
+            <p className="price">
+              <img src={ethIcon} alt="#" className='ethIcon' /> 0.041 ETH
+            </p>
+            <p className="duration">
+              <img src={clock} alt="" className="clock" />
+              3 days left
+            </p>
+          </div>
+          <div className="author">
+            <img src={avatar} alt="" className="author" />
+            <p className="authName"><span className="authSpan">Creation of</span> Jules Wyvern</p>
+          </div>
+        </div>
+      {/*
 
-Our Equilibrium collection promotes balance and calm.
-
-0.041 ETH
-3 days left
-
-Creation of Jules Wyvern
+Creation of Jules Wyvern */}
       </div>
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge"      target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/lexzee">Lexzee</a>.
       </div>
     </>
   )
