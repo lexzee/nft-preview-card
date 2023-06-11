61d14613e6f9f4d6e9a55ede13ce9b984a800e48
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-11 13:44:13 +0100

    finished styling for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,22 @@ function App() {
             Our Equilibrium collection promotes balance and calm.
           </p>
           <div className="priceDuration">
-            <p className="price">
+            {/* <div className="price">
               <img src={ethIcon} alt="#" className='ethIcon' /> 0.041 ETH
-            </p>
-            <p className="duration">
+            </div>
+            <div className="duration">
               <img src={clock} alt="" className="clock" />
               3 days left
-            </p>
+            </div> */}
+            <img src={ethIcon} alt="" className="ethIcon" />
+            <p className="price">0.041 ETH</p>
+            <img src={clock} alt="" className="duration" />
+            <p className="time">3 days left</p>
           </div>
-          <div className="author">
-            <img src={avatar} alt="" className="author" />
-            <p className="authName"><span className="authSpan">Creation of</span> Jules Wyvern</p>
-          </div>
+        </div>
+        <div className="author">
+          <img src={avatar} alt="" className="authorImg" />
+          <p className="authName"><span className="authSpan">Creation of</span> Jules Wyvern</p>
         </div>
       {/*
 
